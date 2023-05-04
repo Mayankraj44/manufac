283896250c6f8fd0f982a5a4cd94ed83922ca1af
Mayank Raj <lui.chang420@gmail.com>
2023-05-04 17:15:43 +0530

    ui completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,116 @@
 import wineData from "./utils/wineData.json";
-import { filterDataIntoCategory } from "./utils/helper.js";
-import { useEffect } from "react";
-function App() {
-  useEffect(() => {
-    filterDataIntoCategory(wineData);
-  }, []);
+import {
+  calculateMean,
+  calculateMedian,
+  calculateMode,
+  filterDataIntoCategory,
+} from "./utils/helper.js";
+import { useEffect, useState } from "react";
 
-  return <div></div>;
+type dataType = {
+  Ash: number | string;
+  Magnesium: string | number;
+  Hue: string | number;
+  Flavanoids: string | number;
+};
+
+function App() {
+  const [data, setData] = useState<{ name: string; data: [dataType] }[]>([]);
+  useEffect(() => {
+    const newdata = filterDataIntoCategory(wineData);
+    setData(newdata);
+  }, []);
+  const rowFlavanoids = [
+    "Measure",
+    "Flavanoids Mean",
+    "Flavanoids Median",
+    "Flavanoids Mode",
+  ];
+  const rowGamma = ["Measure", "Gamma Mean", "Gamma Median", "Gamma Mode"];
+  function calculateGamma(item: {
+    Ash: number | string;
+    Magnesium: string | number;
+    Hue: string | number;
+  }) {
+    return (
+      (Number(item?.Ash) * Number(item.Hue)) /
+      Number(item?.Magnesium)
+    ).toFixed(3);
+  }
+
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <div>
+          {rowFlavanoids?.map((item) => (
+            <div className="cell">{item}</div>
+          ))}
+        </div>
+
+        {data?.map((item) => (
+          <div>
+            <div className="cell values">{item.name}</div>
+            <div className="cell values">
+              {calculateMean(
+                item?.data?.map((item) => Number(item?.Flavanoids))
+              )}
+            </div>
+            <div className="cell values">
+              {calculateMedian(
+                item?.data?.map((item) => Number(item?.Flavanoids))
+              )}
+            </div>
+            <div className="cell values">
+              {calculateMode(
+                item?.data?.map((item) => Number(item?.Flavanoids))
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+      <br />
+      <br />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          {rowGamma?.map((item) => (
+            <div className="cell">{item}</div>
+          ))}
+        </div>
+
+        {data?.map((item) => (
+          <div>
+            <div className="cell values">{item.name}</div>
+            <div className="cell values">
+              {calculateMean(
+                item?.data?.map((item) => Number(calculateGamma(item)))
+              )}
+            </div>
+            <div className="cell values">
+              {calculateMedian(
+                item?.data?.map((item) => Number(calculateGamma(item)))
+              )}
+            </div>
+            <div className="cell values">
+              {calculateMode(
+                item?.data?.map((item) => Number(calculateGamma(item)))
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
