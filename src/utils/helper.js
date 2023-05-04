52f61ece0ee3fcf96f91371bf19d91c3e06a3426
Mayank Raj <lui.chang420@gmail.com>
2023-05-04 17:15:04 +0530

    helper functions added

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -2,10 +2,54 @@ export function filterDataIntoCategory(data) {
   const map = {};
   data?.map((wine) => {
     if (map.hasOwnProperty(wine?.Alcohol)) {
-      map[wine?.Alcohol].push(wine);
+      map[wine?.Alcohol].data.push(wine);
     } else {
-      map[wine?.Alcohol] = [wine];
+      map[wine?.Alcohol] = {
+        name: `Class ${wine?.Alcohol}`,
+        data: [wine],
+      };
     }
   });
-  console.log(map);
+  const result = Object.values(map);
+  return result;
+}
+export function calculateMean(dataset) {
+  const sum = dataset?.reduce((acc, data) => acc + data, 0);
+  return (sum / dataset.length).toFixed(3);
+}
+export function calculateMedian(dataset) {
+  const result = dataset.sort((a, b) => a - b);
+  const length = result.length;
+  let term;
+  if (length % 2 == 0) {
+    term = result[length / 2] + result[length / 2 - 1];
+    return term / 2;
+  } else {
+    term = result[(length - 1) / 2];
+    return term;
+  }
+}
+export function calculateMode(dataset) {
+  let max = 0;
+  const map = dataset?.reduce((acc, data) => {
+    if (acc.hasOwnProperty(data)) {
+      acc[data] += 1;
+    } else {
+      acc[data] = 1;
+    }
+    if (acc[data] > max) {
+      max = acc[data];
+    }
+    return acc;
+  }, {});
+
+  const resultant = Object.entries(map)
+    .filter((a) => a[1] === max)
+    .map((item) => item[0]);
+
+  if (resultant.length > 1) {
+    return "Not found";
+  }
+
+  return resultant;
 }
